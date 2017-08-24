0951921397d90b8d5a4266d775f439caa3a3e6f5
张锦江 <zhangjinjiang@zhangjinjiangdeMacBook-Pro.local>
2017-08-24 16:45:11 +0800

    Initial Commit

---
+++ ZJJCustomTableViewLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJJCustomTableViewLine
+//
+//  Created by 张锦江 on 2017/8/24.
+//  Copyright © 2017年 ZJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
